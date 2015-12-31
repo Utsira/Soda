d46ffba4ee5efd5a483bdea899a60846a29d5492
Puffin & Turtle Software <puffinandturtle@gmail.com>
2015-12-31 11:35:00 +0000

    More robust installer

--- Soda Installer.lua
+++ Soda Installer.lua
@@ -1,15 +1,12 @@
 local url = "https://raw.githubusercontent.com/Utsira/Soda/master/tabs/"
  
-
 local function install(data)
     --parse plist into list of tab files
     local array = data:match("<key>Buffer Order</key>%s-<array>(.-)</array>")
-    local files = {}
-    
+    local files = {}   
     for tabName in array:gmatch("<string>(.-)</string>%s") do
         table.insert(files, {name = tabName})
     end   
-    
     --success function
     local function success(i, name, data)
         if not data then alert("No data", name) return end
@@ -17,25 +14,25 @@ local function install(data)
         files[i].data = data
         for i,v in ipairs(files) do
             if not v.data then 
-                return --quit this function if any files have mssing data
+                return --quit this function if any files have missing data
             end
         end
         --if all data is present then save...
         for i,v in ipairs(files) do
             saveProjectTab(v.name, v.data)
             print("Saved "..i.."/"..#files..":"..v.name)
+            load(v.data)() --load...
         end
-        for i,v in ipairs(files) do --load...
-            load(v.data)()
-        end
-        setup() --and run
+        setup() --...and run
     end
     --request all the tab files
-    for i,v in ipairs(files) do
-        http.request(url..v.name..".lua", function(data) success(i, v.name, data) end, function(error) alert(error, v.name.." not found") end)
+    for i,v in ipairs(files) do 
+        local function retry(error) --try each file twice, in case of time-outs
+            print(error, v.name.." not found, retrying")
+            http.request(url..v.name..".lua", function(data) success(i, v.name, data) end, function(error2) alert(error2, v.name.." not found") end)
+        end
+        http.request(url..v.name..".lua", function(data) success(i, v.name, data) end, retry)
     end
-     
 end
-
 http.request(url.."Info.plist", install, function (error) alert(error) end)
   
